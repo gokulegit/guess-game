4411d0a5ad246ef8452c42a740d41de5575da4b0
gelumalai <Gokul_E@intuit.com>
2020-04-01 19:51:15 +0530

    remove hr from Game grid

--- src/components/GameGrid.js
+++ src/components/GameGrid.js
@@ -3,8 +3,7 @@ import React from 'react';
 
 const GameGrid = ({ attempts }) => {
     return <div>
-        <hr />
-        <table style={{ margin: `0 auto` }}>
+        <table border="1" style={{ borderCollapse: `collapse`, margin: `0 auto` }}>
             <thead>
                 <tr>
                     <td>Attempt</td>
